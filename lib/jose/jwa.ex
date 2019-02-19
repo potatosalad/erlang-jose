1cc90ead96648f2e1ed4a4c2c99b7aaff0463126
Josh Austin <josh.austin@gmail.com>
2019-02-19 17:36:53 -0500

    update docs according to RFC7518 and pkeyutl/evp_pkey_ctrl_str

--- lib/jose/jwa.ex
+++ lib/jose/jwa.ex
@@ -101,7 +101,7 @@ defmodule JOSE.JWA do
   ## Options
 
     * `:rsa_padding` - one of `:rsa_pkcs1_pss_padding` or `:rsa_pkcs1_padding`
-    * `:rsa_pss_saltlen` - sets the salt length for `:rsa_pkcs1_pss_padding`, defaults to `-2`
+    * `:rsa_pss_saltlen` - sets the salt length for `:rsa_pkcs1_pss_padding`, defaults to `-1`
       * `-2` - use maximum for salt length
       * `-1` - use hash length for salt length
       * any number higher than `-1` is used as the actual salt length
@@ -115,7 +115,7 @@ defmodule JOSE.JWA do
 
     * `:rsa_padding` - one of `:rsa_pkcs1_pss_padding` or `:rsa_pkcs1_padding`
     * `:rsa_pss_saltlen` - sets the salt length for `:rsa_pkcs1_pss_padding`, defaults to `-2`
-      * `-2` - use maximum for salt length
+      * `-2` - automatically determine based on the PSS block structure
       * `-1` - use hash length for salt length
       * any number higher than `-1` is used as the actual salt length
   """
