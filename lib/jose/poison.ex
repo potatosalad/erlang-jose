4b1ab146daf6509672175585c45b7c42eaf7e467
Thiago Santos <thia.mdossantos@gmail.com>
2020-11-21 19:53:00 -0300

    Conditional compile Poison-related modules

--- lib/jose/poison.ex
+++ lib/jose/poison.ex
@@ -1,14 +1,16 @@
-defmodule JOSE.Poison do
-  alias JOSE.Poison.LexicalEncoder
+if Code.ensure_loaded?(Poison) do
+  defmodule JOSE.Poison do
+    alias JOSE.Poison.LexicalEncoder
 
-  @spec lexical_encode!(LexicalEncoder.t(), LexicalEncoder.options()) :: iodata | no_return
-  def lexical_encode!(value, options \\ %{}) do
-    iodata = LexicalEncoder.encode(value, options)
+    @spec lexical_encode!(LexicalEncoder.t(), LexicalEncoder.options()) :: iodata | no_return
+    def lexical_encode!(value, options \\ %{}) do
+      iodata = LexicalEncoder.encode(value, options)
 
-    unless options[:iodata] do
-      iodata |> IO.iodata_to_binary()
-    else
-      iodata
+      unless options[:iodata] do
+        iodata |> IO.iodata_to_binary()
+      else
+        iodata
+      end
     end
   end
 end
