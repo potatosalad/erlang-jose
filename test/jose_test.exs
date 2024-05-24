23fe32809320f18c327fe93253c5a15a13d96e40
Jonatan Männchen <jonatan@maennchen.ch>
2024-05-24 16:03:58 +0200

    Support OTP json module (#157)

--- test/jose_test.exs
+++ test/jose_test.exs
@@ -116,6 +116,18 @@ defmodule JOSETest do
     assert binary == :erlang.element(2, JOSE.JWE.to_binary(jwe))
     assert jwe == JOSE.JWE.from_binary(binary)
     assert jwe == JOSE.JWE.from(jwe)
+
+    if System.otp_release() >= "27" do
+      # OTP json
+      JOSE.json_module(:json)
+      assert :jose_json_otp == JOSE.json_module()
+      assert map == :erlang.element(2, JOSE.JWE.to_map(jwe))
+      assert binary == :erlang.element(2, JOSE.JWE.to_binary(jwe))
+      assert jwe == JOSE.JWE.from_binary(binary)
+      assert jwe == JOSE.JWE.from(jwe)
+    end
+
+    # jsx
     # # jiffy
     # JOSE.json_module(:jiffy)
     # assert :jose_json_jiffy == JOSE.json_module()
